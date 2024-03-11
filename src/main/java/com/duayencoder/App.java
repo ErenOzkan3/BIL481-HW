c8876bbaa771f1f1f310ccb83ba0eda40b52c498
Duayen Coder <sasiifanii@gmail.com>
2024-03-11 15:03:23 +0300

    Removed App.java

---
+++ src/main/java/com/duayencoder/App.java
@@ -0,0 +1,11 @@
+package com.duayencoder;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void makeSomeMeaningfulComputationOnSetOfString(){
+
+    }
+}
